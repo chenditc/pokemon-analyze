b5ee53d0a28a315504224f53f7a95b7df7098499
Ubuntu <ubuntu@ip-172-31-6-85.us-west-2.compute.internal>
2016-07-19 02:56:15 +0000

    Add parse output

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install git
+sudo apt-get install python-setuptools python-dev build-essential 
+sudo easy_install pip
+sudo pip install bs4
+sudo pip install lxml
+sudo apt-get install python-lxml
+sudo apt-get install autoconf automake libtool curl make g++ unzip
+sudo apt-get install protobuf-compiler
+sudo apt-get install python-protobuf
--- parse_request_history.py
+++ parse_request_history.py
@@ -1,32 +1,52 @@
 #!/usr/bin/python
 
+from subprocess import Popen, PIPE
+
 import base64
 from bs4 import BeautifulSoup
+import pandas
 
-from subprocess import Popen, PIPE
+import POGOProtos_pb2
 
 def remove_header(message_body):
     index = message_body.find("deflate")
     protoc_buff = message_body[index+11:]
     return protoc_buff
 
-def decode_protoc_buff(protoc_buff):
-    p = Popen(["protoc", "--decode_raw"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    result = p.communicate(protoc_buff)[0]
-    print result
-    quit()
+def decode_response_envelope(protoc_buff):
+    message = POGOProtos_pb2.Networking.Envelopes.ResponseEnvelope()
+    message.ParseFromString(protoc_buff)
+    return message
 
+def decode_request_envelope(protoc_buff):
+    message = POGOProtos_pb2.Networking.Envelopes.RequestEnvelope()
+    message.ParseFromString(protoc_buff)
+    return message
 
 with open('./request_history', 'r') as input_file:
     xml_doc = input_file.read()
     soup = BeautifulSoup(xml_doc, 'xml')
     items = soup.items.find_all("item")
 
+
     for item in items:
         comment = item.comment.getText()
+        url = item.url.getText()
+        if "pgorelease.nianticlabs.com" not in url:
+            continue
+
         request_base64 = item.request.getText()
         request = base64.b64decode(request_base64)
         response_base64 = item.request.getText()
         response = base64.b64decode(response_base64)
 
-        request_body = decode_protoc_buff(remove_header(request)) 
+        request_body = decode_request_envelope(remove_header(request)) 
+        response_body = decode_response_envelope(remove_header(response))
+
+        print "================================================="
+        print("Comment: {0}".format(comment))
+        print(request_body)
+        print "================================================="
+        print(response_body)
+        print("-------------------------------------------------")
+
